02ef82b73bb9ef0ec465b43c71b128264e2b9aa6
torres005 <109883423+torres005@users.noreply.github.com>
2023-08-01 17:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function createTable() {
     //Write your code here
+	 const row = parseInt(prompt("Input No. of Rows"));
+    const column = parseInt(prompt("Input No. of Columns"));
+
+
+    var input = document.getElementById("myTable")
+    input.innerHTML = '';
+	
+    for(i=0;i<row;i++){
+        var rows =  document.createElement('tr')
+
+        for(j =0;j<column;j++){
+            var cell = document.createElement('td');
+            cell.textContent = "Row-"+ i + "Column-"+ j 
+            cell.appendChild(rows)
+        }
+
+        input.appendChild(row);
+    }
 	
 }
