395f30e53c6388950c9eada4fca52fd70ace75a3
torres005 <109883423+torres005@users.noreply.github.com>
2023-08-01 17:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createTable() {
     //Write your code here
-    const row = parseInt(prompt("Input No. of Rows"));
-    const column = parseInt(prompt("Input No. of Columns"));
+    const row = parseInt(prompt("Input number of rows"));
+    const column = parseInt(prompt("Input number of columns"));
 
 
     var input = document.getElementById("myTable")
